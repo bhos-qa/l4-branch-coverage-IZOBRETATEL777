d82d0f9b3fe93f782422387781b8dccfb530f537
Tolstosheyev Roman <izobretatel777@mail.ru>
2023-10-01 11:21:23 +0400

    add controller

--- app/src/main/java/com/example/Main.java
+++ app/src/main/java/com/example/Main.java
@@ -1,50 +1,34 @@
 package com.example;
 
 import com.example.config.JpaConfig;
+import com.example.controller.StudentController;
 import com.example.dao.entity.Student;
 import com.example.dao.repo.StudentRepo;
 import com.example.dao.repo.StudentRepoImpl;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         EntityManagerFactory emf = JpaConfig.getEntityManagerFactory();
-        EntityManager em = emf.createEntityManager();
+        EntityManager entityManager = emf.createEntityManager();
+        StudentRepo studentRepo = new StudentRepoImpl(entityManager);
+        Scanner scanner = new Scanner(System.in);
+        StudentController studentController = new StudentController(studentRepo, scanner);
 
-        StudentRepo studentRepo = new StudentRepoImpl(em);
-        System.out.println("All students: " + studentRepo.getAllStudents());
+        studentController.getAllStudents();
+        studentController.addStudent();
+        studentController.addStudent();
+        studentController.addStudent();
+        studentController.updateStudent();
+        studentController.deleteStudent();
+        studentController.getStudentById();
+        studentController.getStudentByEmail();
 
-        Student student = Student.builder()
-                .name("John Doe")
-                .email("joe.doe@mail.ru")
-                .age((byte) 25)
-                .build();
-        studentRepo.addStudent(student);
-        System.out.println("All students: " + studentRepo.getAllStudents());
-
-
-        Student student1 = studentRepo.getStudentById(1L);
-        student1.setName("Jane Doe");
-        studentRepo.updateStudent(student1);
-        System.out.println("All students: " + studentRepo.getAllStudents());
-
-
-        Student student2 = Student.builder()
-                .name("Mary Doe")
-                .email("mary.doe@mail.ru")
-                .age((byte) 21)
-                .build();
-
-        studentRepo.addStudent(student2);
-        System.out.println("All students: " + studentRepo.getAllStudents());
-
-
-        studentRepo.deleteStudent(1L);
-        System.out.println("All students: " + studentRepo.getAllStudents());
-
-        em.close();
+        entityManager.close();
         JpaConfig.closeEntityManagerFactory(emf);
 
     }
---
+++ app/src/main/java/com/example/controller/StudentController.java
@@ -0,0 +1,70 @@
+package com.example.controller;
+
+import com.example.dao.entity.Student;
+import com.example.dao.repo.StudentRepo;
+import lombok.AllArgsConstructor;
+
+import java.util.Scanner;
+
+@AllArgsConstructor
+public class StudentController {
+    private final StudentRepo studentRepo;
+
+    private final Scanner scanner;
+
+    public void getAllStudents() {
+        System.out.println("All students: " + studentRepo.getAllStudents());
+    }
+
+    public void addStudent() {
+        Student student = new Student();
+        System.out.println("Enter student name: ");
+        student.setName(scanner.nextLine());
+        System.out.println("Enter student email: ");
+        student.setEmail(scanner.nextLine());
+        System.out.println("Enter student age: ");
+        while (!scanner.hasNextByte()) {
+            System.out.println("Please enter a valid byte for age: ");
+            scanner.next();
+        }
+        student.setAge(scanner.nextByte());
+        scanner.nextLine();
+        studentRepo.addStudent(student);
+        System.out.println("All students: " + studentRepo.getAllStudents());
+    }
+
+    public void updateStudent() {
+        Student student = new Student();
+        System.out.println("Enter student id: ");
+        student.setId(scanner.nextLong());
+        System.out.println("Enter student name: ");
+        student.setName(scanner.nextLine());
+        System.out.println("Enter student email: ");
+        student.setEmail(scanner.nextLine());
+        while (!scanner.hasNextByte()) {
+            System.out.println("Enter student age: ");
+            scanner.next();
+        }
+        student.setAge(scanner.nextByte());
+        scanner.nextLine();
+        studentRepo.updateStudent(student);
+        System.out.println("All students: " + studentRepo.getAllStudents());
+    }
+
+
+    public void deleteStudent() {
+        System.out.println("Enter student id: ");
+        studentRepo.deleteStudent(scanner.nextLong());
+        System.out.println("All students: " + studentRepo.getAllStudents());
+    }
+
+    public void getStudentById() {
+        System.out.println("Enter student id: ");
+        System.out.println("Student: " + studentRepo.getStudentById(scanner.nextLong()));
+    }
+
+    public void getStudentByEmail() {
+        System.out.println("Enter student email: ");
+        System.out.println("Student: " + studentRepo.getStudentByEmail(scanner.nextLine()));
+    }
+}
